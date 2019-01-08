524a2abb68fe1c590542f28c48f72fde233e97eb
interglobalmedia <interglobalmedia@gmail.com>
2019-01-08 08:57:10 -0500

    Add back old favicon setup with static

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -83,6 +84,9 @@ module.exports = env => {
             new MiniCssExtractPlugin({
                 filename: PLATFORM === 'production' ? '[name].[hash].css' : '[name].css'
             }),
+            new CopyWebpackPlugin([
+                {from: 'src/static'}
+            ]),
             new WorkboxPlugin.GenerateSW({
                 swDest: 'sw.js'
             }),
